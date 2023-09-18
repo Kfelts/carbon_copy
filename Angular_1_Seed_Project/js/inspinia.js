9a99f246909ae08c2f4b94012b266b6d29e1b556
Kfelts <51497445+Kfelts@users.noreply.github.com>
2023-09-17 21:41:02 -0400

    update Jquery to 3.1.1 standards

--- Angular_1_Seed_Project/js/inspinia.js
+++ Angular_1_Seed_Project/js/inspinia.js
@@ -1,33 +1,34 @@
 /**
- * INSPINIA - Responsive Admin Theme
+ * Responsive Admin Theme
  * 2.7.1
  *
  * Custom scripts
  */
 
-$(document).ready(function () {
+
+$(function () {
 
 
     // Full height of sidebar
 // sourcery skip: avoid-function-declarations-in-blocks
     function fix_height() {
-        var heightWithoutNavbar = $("body > #wrapper").height() - 61;
+        const heightWithoutNavbar = $("#wrapper").height() - 61;
         $(".sidebar-panel").css("min-height", heightWithoutNavbar + "px");
 
-        var navbarHeight = $('nav.navbar-default').height();
-        var wrapperHeight = $('#page-wrapper').height();
+        const navbarHeight = $('nav.navbar-default').height();
+        const wrapperHeight = $('#page-wrapper').height();
 
         if(navbarHeight > wrapperHeight){
-            $('#page-wrapper').css("min-height", navbarHeight + "px");
+            $('#page-wrapper').css("min-height", navbarHeight - 60 + "px");
         }
 
         if(navbarHeight < wrapperHeight){
-            $('#page-wrapper').css("min-height", $(window).height()  + "px");
+            $('#page-wrapper').css("min-height", $(window).height() - 60  + "px");
         }
 
         if ($('body').hasClass('fixed-nav')) {
             if (navbarHeight > wrapperHeight) {
-                $('#page-wrapper').css("min-height", navbarHeight  + "px");
+                $('#page-wrapper').css("min-height", navbarHeight - 60  + "px");
             } else {
                 $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
             }
@@ -35,14 +36,14 @@ $(document).ready(function () {
 
     }
 
-    $(window).bind("load resize scroll", function() {
+    $(window).on("load resize scroll", function() {
         if(!$("body").hasClass('body-small')) {
                 fix_height();
         }
     });
 
     // Move right sidebar top after scroll
-    $(window).scroll(function(){
+    $(window).on("scroll", function(){
         if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav') ) {
             $('#right-sidebar').addClass('sidebar-top');
         } else {
@@ -50,14 +51,14 @@ $(document).ready(function () {
         }
     });
 
-    setTimeout(function(){
-        fix_height();
-    });
+ //   setTimeout(function(){
+ //       fix_height();
+  //  });
 
 });
 
 // Minimalize menu when screen is less than 768px
-$(window).bind("load resize", function () {
+$(window).on("load resize", function () {
     if ($(document).width() < 769) {
         $('body').addClass('body-small')
     } else {
